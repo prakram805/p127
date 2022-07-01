e1c9673de29f752ab4f4780d8a89a15250966639
prakram805 <70887295+prakram805@users.noreply.github.com>
2022-07-01 07:16:19 +0000

    Add files via upload

---
+++ project.py
@@ -0,0 +1,36 @@
+
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+import requests
+bright_stars_url="https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+page=requests.get(bright_stars_url)
+print(page)
+soup=bs(page.text,'html.parser')
+star_table=soup.find("table")
+temp_list=[]
+table_rows=star_table.find_all("tr")
+for tr in table_rows:
+    td=tr.find_all("td")
+    row=[i.text.rstrip()for i in td]
+    temp_list.append(row)
+star_name=[]
+distance=[]
+mass=[]
+radius=[]
+lum=[]
+for i in range(1,len(temp_list)):
+    star_name.append(temp_list[i][1])
+    distance.append(temp_list[i][3])
+    mass.append(temp_list[i][5])
+    radius.append(temp_list[i][6])
+    lum.append(temp_list[i][7])
+df2=pd.DataFrame(list(zip(star_name,distance,mass,lum,radius)),columns=['star_name','distance','mass','lum','radius'])
+print(df2)
+df2.to_csv("bright_stars.csv")
+
+
+
+
+
+
